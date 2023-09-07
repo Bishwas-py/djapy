2558f1231a04b6e753712cd07a75a0921b173d49
Tej Magar <tejmagar20510@gmail.com>
2023-09-07 16:44:57 +0545

    added typehint

--- djapy/mixins/permissions.py
+++ djapy/mixins/permissions.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from djapy.views.generics import DjapyBaseView
+from django.http import HttpResponse
 
 
 class PermissionMixin(DjapyBaseView, ABC):
@@ -8,7 +9,7 @@ class PermissionMixin(DjapyBaseView, ABC):
     def has_permission(self, request) -> bool:
         pass
 
-    def __render__(self, request):
+    def __render__(self, request) -> HttpResponse:
         if not self.has_permission(request):
             print('no permission')
 
