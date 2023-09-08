653ae996fd623391fa76252c0915596bd6d6934f
codix <bishwasbh@gmail.com>
2023-09-08 20:02:32 +0545

    Changed to Version 4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="djapy",
-    version="0.0.3",
+    version="0.0.4",
     description="Do not quit using Django just because of Django Rest Framework, Django is JSOMe",
     long_description=long_description,
     long_description_content_type="text/markdown",
