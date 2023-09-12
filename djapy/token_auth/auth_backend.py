f817388157f5d2fa36a362c51f443ccb83048513
Tej Magar <tejmagar20510@gmail.com>
2023-09-12 09:01:44 +0545

    added authentication backend

--- djapy/token_auth/auth_backend.py
+++ djapy/token_auth/auth_backend.py
@@ -1,21 +1,19 @@
-from django.contrib.auth import login, logout
-
-from .models import AuthToken
+from ..auth.backends import BaseAuthentication
 from ..exceptions import AuthBackendException
+from .models import AuthToken
 
 
-class TokenAuth:
+class TokenAuth(BaseAuthentication):
     def authenticate(self, request):
         authorization_token = request.headers.get('Authorization')
         if not authorization_token:
-            logout(request)
             return
 
         authorization_token = authorization_token.replace('Token', '').strip()
 
         try:
             auth_token = AuthToken.objects.get(key__exact=authorization_token)
+            return auth_token.user
         except AuthToken.DoesNotExist:
             raise AuthBackendException('Invalid auth token')
 
-        login(request, auth_token.user)
