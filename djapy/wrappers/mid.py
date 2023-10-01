737bf3bf081d457fbee62b11cd70fcb119ec7c24
bishwas-py <bishwasbh@gmail.com>
2023-10-01 17:31:05 +0545

    HandleErrorMiddleware added

---
+++ djapy/wrappers/mid.py
@@ -0,0 +1,28 @@
+from django.http import JsonResponse
+from djapy.utils.prepare_exception import log_exception
+from djapy.utils.response_format import create_response
+
+
+class HandleErrorMiddleware:
+    """
+    Middleware to handle exceptions and return a JsonResponse.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception):
+        error, display_message = log_exception(request, exception)
+        error_response = create_response(
+            'failed',
+            'server_error',
+            message=display_message,
+            extras={
+                'path': request.path,
+                'error': error,
+            }
+        )
+        return JsonResponse(error_response, status=500)
