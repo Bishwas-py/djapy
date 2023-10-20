e544562f16c19ff73dd83ef9d7222fa65d0ff20b
bishwas-py <bishwasbh@gmail.com>
2023-10-20 15:46:12 +0545

    UHandleErrorMiddleware for upper error handling

--- djapy/wrappers/mid.py
+++ djapy/wrappers/mid.py
@@ -13,17 +13,6 @@ class HandleErrorMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        if response.status_code >= 400 and not isinstance(response, JsonResponse):
-            error_response = create_json(
-                'failed',
-                'u_server_error',
-                message="An error occurred while processing your request.",
-                extras={
-                    'reason': response.reason_phrase,
-                    'path': request.path,
-                }
-            )
-            return JsonResponse(error_response, status=response.status_code)
         return response
 
     @staticmethod
@@ -39,3 +28,27 @@ class HandleErrorMiddleware:
             }
         )
         return JsonResponse(error_response, status=500)
+
+
+class UHandleErrorMiddleware:
+    """
+    Middleware to handle exceptions and return a JsonResponse.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if response.status_code >= 400 and not isinstance(response, JsonResponse):
+            error_response = create_json(
+                'failed',
+                'u_server_error',
+                message="An error occurred while processing your request.",
+                extras={
+                    'reason': response.reason_phrase,
+                    'path': request.path,
+                }
+            )
+            return JsonResponse(error_response, status=response.status_code)
+        return response
