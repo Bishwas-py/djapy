58aa643ba8c715bb2eaad158a81f2a9c6df1d6a6
Bishwas Bhandari <bishwasbh@gmail.com>
2024-02-25 18:24:41 +0545

    core renamed

---
+++ djapy/__init__.py
@@ -0,0 +1,5 @@
+from .core import djapy_method, djapy_login_required
+from .core.dec import djapify
+from .core.openapi import openapi
+
+__all__ = ['djapify', 'openapi']
--- djapy/v2/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ['djapify', 'djapy_login_required', 'djapy_method']
-
-from djapy.v2.auth_dec import djapy_login_required, djapy_method
-from djapy.v2.dec import djapify
