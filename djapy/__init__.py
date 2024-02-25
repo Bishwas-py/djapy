37a3372f45207fdfeabe0d173c26a7d0ca4a0b08
Bishwas Bhandari <bishwasbh@gmail.com>
2024-02-25 18:26:27 +0545

    default exports added

--- djapy/__init__.py
+++ djapy/__init__.py
@@ -1,5 +1,3 @@
-from .core import djapy_method, djapy_login_required
-from .core.dec import djapify
-from .core.openapi import openapi
+from .core import djapy_method, djapy_login_required, openapi, djapify
 
-__all__ = ['djapify', 'openapi']
+__all__ = ['djapify', 'openapi', 'djapy_login_required', 'djapy_method']
