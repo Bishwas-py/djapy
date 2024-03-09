c4fde4964e8cf6d3c64871a1cea5c68aa3385565
Bishwas Bhandari <bishwasbh@gmail.com>
2024-03-09 09:02:30 +0545

    Offset Limit pagination added

--- djapy/core/pagination/pagination.py
+++ djapy/core/pagination/pagination.py
@@ -1,3 +1,5 @@
+import math
+
 from django.core.paginator import Paginator, EmptyPage
 from typing import Generic, TypeVar
 
@@ -9,20 +11,37 @@ from djapy.schema import Schema
 T = TypeVar('T')
 
 
+class BasicPagination:
+    """
+    Basic pagination with no features implemented. This is the structure of the pagination.
+    """
+    query = []
+
+    class response(Schema, Generic[T]):
+        pass
+
+
 class OffsetLimitPagination:
     """Pagination based on offset and limit."""
 
     query = [
-        ('offset', conint(ge=1), 10),
-        ('limit', conint(gt=0), 1)
+        ('offset', conint(ge=0), 0),
+        ('limit', conint(gt=0), 10)
     ]
 
+    def __repr__(self):
+        return f"OffsetLimitPagination({self.query})"
+
     class response(Schema, Generic[T]):
-        result: T
+        items: T
         offset: int
         limit: int
         has_next: bool
         has_previous: bool
+        total_pages: int
+
+        def __repr__(self):
+            return f"{T.__name__} with offset {self.offset} and limit {self.limit}"
 
         @model_validator(mode="before")
         def make_data(cls, queryset, info):
@@ -33,16 +52,16 @@ class OffsetLimitPagination:
             offset = input_data['offset']
             limit = input_data['limit']
 
-            pagination = Paginator(queryset, limit)
-            try:
-                page = pagination.page(offset)
-            except EmptyPage:
-                raise ValueError("Input data limit exceeded available data.")
+            if len(queryset) == 0 or offset > len(queryset):
+                return []
+
+            queryset_subset = queryset[offset:offset + limit]
 
             return {
-                "result": page.object_list,
-                "offset": page.number,
-                "limit": page.paginator.per_page,
-                "has_next": page.has_next(),
-                "has_previous": page.has_previous()
+                "items": queryset_subset,
+                "offset": offset,
+                "limit": limit,
+                "has_next": len(queryset_subset) == limit,
+                "has_previous": offset > 0,
+                "total_pages": int(math.ceil(len(queryset) / limit)),
             }
