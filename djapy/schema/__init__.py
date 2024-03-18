147a20138509489b612fad669411ef5d60a1a65c
Bishwas Bhandari <bishwasbh@gmail.com>
2024-03-18 19:25:34 +0545

    issue: #19 allowing context in @computed_field

--- djapy/schema/__init__.py
+++ djapy/schema/__init__.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel, model_validator
 
 __all__ = ['Schema', 'payload']
 
+from pydantic_core.core_schema import ValidationInfo
+
 from djapy.core.typing_utils import G_TYPE
 
 
@@ -26,11 +28,16 @@ class SourceAble(BaseModel):
     Allows the model to have a source object.
     """
     _source_obj: Any | None = None
+    _validation_info: ValidationInfo | None = None
+    _context: dict | None = None
 
     @model_validator(mode="wrap")
-    def __validator__(cls, val: Any, next_: typing.Callable[[Any], typing.Self]) -> typing.Self:
+    def __validator__(cls, val: Any, next_: typing.Callable[[Any], typing.Self],
+                      validation_info: ValidationInfo) -> typing.Self:
         obj = next_(val)
         obj._source_obj = val
+        obj._validation_info = validation_info
+        obj._context = validation_info.context
 
         return obj
 
