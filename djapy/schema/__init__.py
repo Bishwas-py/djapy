3d4cdf7b1412fde964de829922b0b77dcc9e8a82
Bishwas Bhandari <bishwasbh@gmail.com>
2024-03-18 17:40:57 +0545

    Sourceable added, #16

--- djapy/schema/__init__.py
+++ djapy/schema/__init__.py
@@ -1,6 +1,7 @@
-from typing import Generic, NewType, Type, TypeAlias, TypeVar
+import typing
+from typing import Generic, NewType, Type, TypeAlias, TypeVar, Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 __all__ = ['Schema', 'payload']
 
@@ -18,6 +19,20 @@ class Schema(BaseModel):
 
     class Info:
         description: dict = {}
+
+
+class SourceAble(BaseModel):
+    """
+    Allows the model to have a source object.
+    """
+    _source_obj: Any | None = None
+
+    @model_validator(mode="wrap")
+    def __validator__(cls, val: Any, next_: typing.Callable[[Any], typing.Self]) -> typing.Self:
+        obj = next_(val)
+        obj._source_obj = val
+
+        return obj
 
 
 class Payload:
