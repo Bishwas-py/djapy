fada0d2ff90d9ea7e7e68f15f703e555bb485db9
Bishwas Bhandari <bishwasbh@gmail.com>
2024-03-21 10:17:04 +0545

    optimized uni_schema

--- djapy/schema/handle.py
+++ djapy/schema/handle.py
@@ -1,33 +1,42 @@
-__all__ = ['payload', 'uni_schema', 'StatusCodes']
+__all__ = ['payload', 'uni_schema', 'status_codes']
 
 from http.client import responses
-from typing import Type, TypedDict, Literal
+from typing import Type, TypedDict
 
 from .schema import Schema
 from djapy.core.typing_utils import G_TYPE
 
 
 class StatusCodes:
-    success_2xx = {code for code in range(200, 300) if code in responses}
-    error_4xx = {code for code in range(400, 500) if code in responses}
-    server_error_5xx = {code for code in range(500, 600) if code in responses}
+    def __init__(self):
+        self.success_2xx = {code for code in range(200, 300) if code in responses}
+        self.error_4xx = {code for code in range(400, 500) if code in responses}
+        self.server_error_5xx = {code for code in range(500, 600) if code in responses}
 
 
 class ReSchema:
-    def __call__(self, schema: Schema | Type[TypedDict] | Literal[*StatusCodes.__annotations__],
+    """
+    This class is used to generate response schemas for different status codes.
+    """
+
+    def __call__(self, schema: Schema | Type[TypedDict],
                  status_set=None):
+        """
+        :param schema: The schema to be used.
+        :param status_set: The status codes to be used. Default is None. If None, all status codes are used `(1xx, 2xx, 3xx, 4xx, 5xx)`.
+        """
         if status_set is None:
-            status_set = StatusCodes.success_2xx
+            status_set = {code for code in responses}
         return {code: schema for code in status_set}
 
     def success_2xx(self, schema):
-        return self.__call__(schema, StatusCodes.success_2xx)
+        return self.__call__(schema, status_codes.success_2xx)
 
     def error_4xx(self, schema):
-        return self.__call__(schema, StatusCodes.error_4xx)
+        return self.__call__(schema, status_codes.error_4xx)
 
     def server_error_5xx(self, schema):
-        return self.__call__(schema, StatusCodes.server_error_5xx)
+        return self.__call__(schema, status_codes.server_error_5xx)
 
 
 class Payload:
@@ -42,4 +51,5 @@ class Payload:
 
 
 payload = Payload()
-uni_schema = ReSchema()
+uni_schema = ReSchema()  # uni_schema is short for unified schema
+status_codes = StatusCodes()
