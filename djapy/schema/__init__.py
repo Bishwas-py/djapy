7cc521784ba809c761d18f4d37a00f0da736d023
Bishwas Bhandari <bishwasbh@gmail.com>
2024-06-24 08:22:01 +0545

    #29 outsource added in schema/init

--- djapy/schema/__init__.py
+++ djapy/schema/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['Schema', 'Form', 'QueryList', 'payload', 'uni_schema']
+__all__ = ['Schema', 'Form', 'QueryList', 'Outsource', 'payload', 'uni_schema']
 
 from djapy.schema.handle import payload, uni_schema
 from djapy.schema.schema import Schema, Outsource, Form, QueryList
