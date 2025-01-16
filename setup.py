444363ceac18e08ac62592c5bb60b37a65d41991
Bishwas Bhandari <bishwasbh@gmail.com>
2025-01-16 11:39:46 +0545

    chore: v updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
    name="djapy",
-   version="0.1.105.dev1",
+   version="0.1.105.dev2",
    description="Fast, zero-boilerplate Django REST API framework with pure Python typing!",
    long_description=long_description,
    long_description_content_type="text/markdown",
