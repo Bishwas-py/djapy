305d2c6346c1cc1071794602d0bffcb59fe36e77
bishwas-py <bishwasbh@gmail.com>
2025-09-30 13:08:43 +0545

    chore: update project version to 0.3.1.dev0 and adjust package structure for improved compatibility

--- setup.py
+++ setup.py
@@ -5,27 +5,36 @@ with open("README.md", "r") as fh:
 
 setup(
    name="djapy",
-   version="0.2.1",
+   version="0.3.1.dev0",
    description="Fast, zero-boilerplate Django REST API framework with pure Python typing!",
    long_description=long_description,
    long_description_content_type="text/markdown",
    url="https://github.com/Bishwas-py/djapy",
-   py_modules=["djapy"],
-   packages=find_packages(include=["djapy", "djapy.*"]),
+   package_dir={"": "src"},
+   packages=find_packages(where="src", include=["djapy", "djapy.*"]),
    package_data={
-      "djapy": ["templates/djapy/*.html"]
+      "djapy": ["templates/djapy/*.html", "py.typed"]
    },
    project_urls={
       "Documentation": "https://djapy.io",
    },
    classifiers=[
+      "Development Status :: 4 - Beta",
       "Programming Language :: Python :: 3",
       "Programming Language :: Python :: 3.10",
       "Programming Language :: Python :: 3.11",
       "Programming Language :: Python :: 3.12",
       "Programming Language :: Python :: 3.13",
       "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-      "Operating System :: OS Independent"
+      "Operating System :: OS Independent",
+      "Framework :: Django",
+      "Framework :: Django :: 3.2",
+      "Framework :: Django :: 4.0",
+      "Framework :: Django :: 4.1",
+      "Framework :: Django :: 4.2",
+      "Framework :: Django :: 5.0",
+      "Intended Audience :: Developers",
+      "Topic :: Software Development :: Libraries :: Python Modules",
    ],
    install_requires=[
       "Django",
